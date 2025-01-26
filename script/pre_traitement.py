892b21860db6ed4889a4fb2d57e3786d19018c10
leonavarrosig66 <leo.navarro@etu.univ-tlse2.fr>
2025-01-26 10:27:45 +0000

    ajout du pre_traitement pour le fichier NDVI

--- script/pre_traitement.py
+++ script/pre_traitement.py
@@ -3,7 +3,7 @@ import sys
 import re
 import geopandas as gpd
 sys.path.append('/home/onyxia/work/projet_901_21/script')
-from my_function import clip_raster, apply_mask, concat_bands
+from my_function import clip_raster, apply_mask, concat_bands, calculate_ndvi
 
 # Initialisation des chemins nécessaires
 raster_folder = "/home/onyxia/work/data/images"
@@ -12,6 +12,8 @@ masque_file = "/home/onyxia/work/projet_901_21/results/data/img_pretraitees/masq
 output_result = "/home/onyxia/work/projet_901_21/results/data/img_pretraitees"
 output_decoupe_folder = "/home/onyxia/work/data/project/pretraitement_decoupe"
 output_masque_folder = "/home/onyxia/work/data/project/pretraitement_masque"
+output_masque_ndvi_folder = "/home/onyxia/work/data/project/pretraitement_masque_ndvi"
+output_ndvi_folder = "/home/onyxia/work/data/project/pretraitement_ndvi" 
 
 # Initialisation des variables nécessaires
 spatial_res = 10  # Résolution spatiale de 10 m
@@ -30,6 +32,11 @@ if not os.path.exists(output_decoupe_folder):
 if not os.path.exists(output_masque_folder):
     os.makedirs(output_masque_folder)
 
+# Créer le dossier de sortie s'il n'existe pas
+if not os.path.exists(output_masque_ndvi_folder):
+   os.makedirs(output_masque_ndvi_folder)
+
+# Traitements pour générer le fichier multibandes concaténés
 # Liste de tous les fichiers .tif dans le dossier de rasters
 raster_files = [f for f in os.listdir(raster_folder) if f.endswith('.tif')]
 
@@ -89,3 +96,37 @@ raster_files_masque = sorted([os.path.join(output_masque_folder, f) for f in os.
 out_result = os.path.join(output_result, "Serie_temp_S2_allbands.tif")
 
 concat_bands(raster_files_masque, out_result, data_type, no_data)
+
+data_type = "Float32"
+no_data = -9999
+expression = '(A - B) / (A + B)'
+# Traitements pour générer le NDVI
+# Liste de tous les fichiers raster découpés
+raster_files_decoupee = [f for f in os.listdir(output_decoupe_folder) if f.endswith('.tif')]
+
+# On boucle pour traiter chaque raster 1 par 1
+for raster_file_decoupee in raster_files_decoupee:
+    # Charger l'image raster
+    raster_decoupee_path = os.path.join(output_decoupe_folder, raster_file_decoupee)
+    
+    # Séparer le nom du fichier de son extension
+    file_name, file_extension = os.path.splitext(raster_file_decoupee)
+    
+    # Créer le nouveau nom de fichier
+    new_file_name = f"{file_name}_masque{file_extension}"
+    
+    # Création du chemin de sortie
+    output_path = os.path.join(output_masque_ndvi_folder, new_file_name)
+    
+    # Appel de la fonction pour mettre en place le masque forêt
+    apply_mask(raster_decoupee_path, masque_file, output_path, data_type, driver, expression, no_data)
+
+# Création des rasters NDVI
+calculate_ndvi(output_masque_ndvi_folder, output_ndvi_folder, expression)
+
+raster_files_ndvi = sorted([os.path.join(output_ndvi_folder, f) for f in os.listdir(output_ndvi_folder) if f.endswith('.tif')])
+
+out_result = os.path.join(output_result, "Serie_temp_S2_ndvi.tif")
+
+# Concaténation des 6 rasters préalablement créés
+concat_bands(raster_files_ndvi, out_result, data_type, no_data)
